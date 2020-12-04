d8cc6667089344d57f3c338615601750b7a9c540
jleomorris <70540466+jleomorris@users.noreply.github.com>
2020-12-04 02:14:46 +0000

    Add FaqSection

---
+++ src/components/FaqSection.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>When can I start?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            fugiat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            fugiat.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What motivates me?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            fugiat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            fugiat.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What are my hobbies?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            fugiat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            fugiat.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>How can you contact me?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            fugiat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            fugiat.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
