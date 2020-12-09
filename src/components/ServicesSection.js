8fd680403e12db1d371ace20e0bccbb4e478d4e9
jleomorris <70540466+jleomorris@users.noreply.github.com>
2020-12-09 01:41:17 +0000

    Update global and ServicesSection styling

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,60 +1,86 @@
 import React from "react";
 import teamwork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
+// Styles
+import { About, Description, Image, Hide } from "../styles";
+import styled from "styled-components";
 
 const ServicesSection = () => {
   return (
-    <div className="services">
-      <div className="description"></div>
-      <h2>
-        <span>Always improving</span> my skills
-      </h2>
-      <div className="cards">
-        <div className="card">
-          <div className="icon">
-            <img src={teamwork} alt="teamwork icon" />
-            <h3>Team player</h3>
-          </div>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam,
-            accusantium.
-          </p>
-        </div>
-        <div className="card">
-          <div className="icon">
-            <img src={teamwork} alt="teamwork icon" />
-            <h3>Experience collaborating with teams</h3>
-          </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, vel!
-          </p>
-        </div>
-        <div className="card">
-          <div className="icon">
-            <img src={teamwork} alt="teamwork icon" />
-            <h3>Keen eye for UI and UX design</h3>
-          </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
-            magni.
-          </p>
-        </div>
-        <div className="card">
-          <div className="icon">
-            <img src={teamwork} alt="teamwork icon" />
-            <h3>Always willing to learn new skills</h3>
-          </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia,
-            saepe.
-          </p>
-        </div>
-      </div>
-      <div className="image">
+    <Services>
+      <Description>
+        <h2>
+          <span>Always improving</span> my skills
+        </h2>
+        <Cards>
+          <Card>
+            <div className="icon">
+              <img src={teamwork} alt="teamwork icon" />
+              <h3>Team player</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={teamwork} alt="teamwork icon" />
+              <h3>Experience collaborating with teams</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={teamwork} alt="teamwork icon" />
+              <h3>Keen eye for UI and UX design</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className="icon">
+              <img src={teamwork} alt="teamwork icon" />
+              <h3>Always willing to learn new skills</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
         <img src={home2} alt="home icon" />
-      </div>
-    </div>
+      </Image>
+    </Services>
   );
 };
+
+const Services = styled(About)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  flex-basis: 20rem;
+  margin-right: 3rem;
+
+  .icon {
+    display: flex;
+    align-items: center;
+
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+`;
 
 export default ServicesSection;
