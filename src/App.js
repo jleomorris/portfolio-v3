85cdbede84facb365fd7303332c0507cc89b7399
jleomorris <70540466+jleomorris@users.noreply.github.com>
2020-12-09 03:25:48 +0000

    Add Nav component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import AboutSection from "./components/AboutSection";
 //Global style
 import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
