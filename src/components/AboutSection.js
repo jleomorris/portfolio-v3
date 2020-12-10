fe5cc404b9a1434cf55641079d85375f07eb189d
jleomorris <70540466+jleomorris@users.noreply.github.com>
2020-12-10 03:11:55 +0000

    Add background to AboutSection

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,13 +1,15 @@
 import React from "react";
 import home1 from "../img/home1.png";
 import aboutImg from "../img/about.jpg";
+import bannerBackground from "../img/banner-full-size.jpg";
 //Styled
 import styled from "styled-components";
 import { About, Description, Image, Hide } from "../styles";
 
 const AboutSection = () => {
   return (
-    <About>
+    <StyledAbout>
+      <div className="img-filter"></div>
       <Description>
         <div className="title">
           <Hide>
@@ -26,15 +28,32 @@ const AboutSection = () => {
           Actively seeking work, interested in development positions in
           London/within 20 miles of Southend on Sea, Essex
         </p>
-        <button>Contact us</button>
+        <button>Contact</button>
       </Description>
       <Image>
         <img src={aboutImg} alt="guy with a camera" />
       </Image>
-    </About>
+    </StyledAbout>
   );
 };
 
 //Styled components
+const StyledAbout = styled(About)`
+  background: url(${bannerBackground});
+  background-size: cover;
+  background-position: top;
+  position: relative;
+  z-index: 0;
+
+  .img-filter {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: #00000066;
+    z-index: -1;
+  }
+`;
 
 export default AboutSection;
