2228a09ebf42847567d1d1cb3ea1f92700cb1fa6
jleomorris <70540466+jleomorris@users.noreply.github.com>
2020-12-10 01:48:55 +0000

    Add router setup for About, Projects and Contact page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
 import AboutSection from "./components/AboutSection";
-//Global style
+// Global style
 import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
+// Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
     </div>
   );
 }
