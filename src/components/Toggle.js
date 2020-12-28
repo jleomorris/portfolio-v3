603e70ce295b0a204558414e68819b3892520cbe
jleomorris <70540466+jleomorris@users.noreply.github.com>
2020-12-28 01:50:45 +0000

    Add chevrons to Faq titles

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,17 +1,41 @@
 import React, { useState } from "react";
 // Framer motion
 import { motion } from "framer-motion";
+// Font Awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+// Styled components
+import styled from "styled-components";
 
 const Toggle = ({ children, title }) => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <motion.div layout className="question" onClick={() => setToggle(!toggle)}>
-      <motion.h4 layout>{title}</motion.h4>
+    <StyledToggle
+      layout
+      className="question"
+      onClick={() => setToggle(!toggle)}
+    >
+      <div className="title-container">
+        <motion.h4 layout>{title}</motion.h4>
+        <FontAwesomeIcon icon={toggle ? faChevronDown : faChevronUp} />
+      </div>
       {toggle ? children : ""}
       <div className="faq-line"></div>
-    </motion.div>
+    </StyledToggle>
   );
 };
+
+const StyledToggle = styled(motion.div)`
+  .title-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    svg {
+      font-size: 2rem;
+    }
+  }
+`;
 
 export default Toggle;
