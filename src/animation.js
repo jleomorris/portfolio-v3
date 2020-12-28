80925cf2fc224f8d43b2e5ffe88fff2eda54bcad
jleomorris <70540466+jleomorris@users.noreply.github.com>
2020-12-28 00:58:20 +0000

    Added new scrollReveal animations

--- src/animation.js
+++ src/animation.js
@@ -64,3 +64,21 @@ export const sliderContainer = {
   hidden: { opacity: 1 },
   show: { opacity: 1, transition: { staggerChildren: 0.15, ease: "easeOut" } },
 };
+
+export const scrollRevealRight = {
+  hidden: { opacity: 0, x: 100, transition: { duration: 0.5 } },
+  show: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 1, ease: "easeOut" },
+  },
+};
+
+export const scrollRevealLeft = {
+  hidden: { opacity: 0, x: -100, transition: { duration: 0.5 } },
+  show: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 1, ease: "easeOut" },
+  },
+};
