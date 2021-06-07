6adfad0484780aa0adf44c1aac420a13ee5aeac2
jleomorris <james-morris2010@hotmail.co.uk>
2021-06-08 00:48:18 +0100

    Update about and faq section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,13 +1,13 @@
-import React from "react";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 // Components
-import { About } from "../styles";
-import Toggle from "./Toggle";
+import { About } from '../styles';
+import Toggle from './Toggle';
 // Framer motion
-import { AnimateSharedLayout, motion } from "framer-motion";
-import { useScroll } from "./useScroll";
-import { scrollRevealRight } from "../animation";
+import { AnimateSharedLayout, motion } from 'framer-motion';
+import { useScroll } from './useScroll';
+import { scrollRevealRight } from '../animation';
 
 const FaqSection = () => {
   const [element, controls] = useScroll();
@@ -20,23 +20,29 @@ const FaqSection = () => {
       <AnimateSharedLayout>
         <motion.div
           variants={scrollRevealRight}
-          initial="hidden"
+          initial='hidden'
           animate={controls}
           ref={element}
         >
-          <Toggle title="What front end experience do I have?">
-            <div className="answer">
+          <Toggle title='What front end experience do I have?'>
+            <div className='answer'>
               <p>
                 From Sept 2019 to Sept 2020 I completed a one year internship at
-                the digital learning company Saffron Interactive — as a Front
-                End Developer and IT Technician. Over this period I gained
-                experience of working on live client projects using React and I
-                also developed a keen eye for UI, UX and responsive design.
+                the digital learning company Saffron Interactive (Front End
+                Developer and IT Technician). Over this period I gained
+                experience of React, working on live client projects and I also
+                developed a keen eye for UI, UX and responsive design.
+              </p>
+              <p>
+                In March 2020 I began a new role at IronmongeryDirect as a React
+                Developer. Since starting here I've strengthened my React and
+                Redux principles and have contributed to multiple new exciting
+                projects in line with a company rebrand.
               </p>
             </div>
           </Toggle>
-          <Toggle title="What are my qualifications?">
-            <div className="answer">
+          <Toggle title='What are my qualifications?'>
+            <div className='answer'>
               <p>
                 I hold a Bsc in Forensic Biology (2:1) and a Msc in Computer
                 Science with Industrial Placement (Merit).
@@ -48,34 +54,33 @@ const FaqSection = () => {
               <p>Notice period for my current employment is 2 months.</p>
             </div>
           </Toggle> */}
-          <Toggle title="What motivates me?">
-            <div className="answer">
+          <Toggle title='What motivates me?'>
+            <div className='answer'>
               <p>
                 My desire to work within the computing industry stemmed from the
                 moment I learnt how to build my first PC (examples of my built
-                systems can be seen on the{" "}
+                systems can be seen on the{' '}
                 <Link to={`${process.env.PUBLIC_URL}/custom-desktops`}>
                   Custom Desktops
-                </Link>{" "}
+                </Link>{' '}
                 page). My fine attention to detail can be seen in the desktops I
                 produce, and this is the same level of focus and creativity I
-                bring when working on projects (examples can be seen on the the{" "}
-                <Link to={`${process.env.PUBLIC_URL}/projects`}>Projects</Link>{" "}
+                bring when working on projects (examples can be seen on the the{' '}
+                <Link to={`${process.env.PUBLIC_URL}/projects`}>Projects</Link>{' '}
                 page).
               </p>
               <p>
                 I'm driven and motivated to become as strong a developer as I
                 can be. I like to maintain a high rate of learning and am always
                 exposing myself to new frameworks, libraries and technologies.
-                At present I am focusing on the ins and outs of React, at some
-                point in the future I'd like to develop my back end skills as
-                I'd like to eventually pursue a career as a full stack
-                developer.
+                At present I am continuing development of my React skills, in
+                the future I'd like to develop my back end skills as I'd like to
+                eventually pursue a career as a full stack developer.
               </p>
             </div>
           </Toggle>
-          <Toggle title="What are my hobbies?">
-            <div className="answer">
+          <Toggle title='What are my hobbies?'>
+            <div className='answer'>
               <p>
                 In my spare time I build PCs for friends and family - I'm
                 currently hooked on small form factor (SFF) PCs. I've also been
@@ -84,11 +89,11 @@ const FaqSection = () => {
               </p>
             </div>
           </Toggle>
-          <Toggle title="How can you contact me?">
-            <div className="answer">
+          <Toggle title='How can you contact me?'>
+            <div className='answer'>
               <p>
-                My contact details can be found on the{" "}
-                <Link to={`${process.env.PUBLIC_URL}/contact`}>Contact</Link>{" "}
+                My contact details can be found on the{' '}
+                <Link to={`${process.env.PUBLIC_URL}/contact`}>Contact</Link>{' '}
                 page.
               </p>
             </div>
