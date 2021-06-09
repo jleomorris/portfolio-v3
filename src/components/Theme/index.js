6e4eb1b991d44bfb279d3c53398f7f0b39ea14ae
jleomorris <james-morris2010@hotmail.co.uk>
2021-06-09 23:45:19 +0100

    Add app theme

---
+++ src/components/Theme/index.js
@@ -0,0 +1,11 @@
+export const theme = {
+  primaryColor: '#acd923',
+  timeline: {
+    background: '#ffffff',
+    fontColor: '#223',
+    icon: {
+      jobColor: '#488447',
+      educationColor: '#63639a',
+    },
+  },
+};
