4acc7269861fe1ed3784f3fee53840892c960879
jleomorris <james-morris2010@hotmail.co.uk>
2021-06-13 22:45:55 +0100

    Update GlobalStyle and Theme

--- src/components/Theme/index.js
+++ src/components/Theme/index.js
@@ -1,5 +1,14 @@
 export const theme = {
   primaryColor: '#dbff6fb5',
+  primaryButtonColor: '#7d9141',
+  primaryButtonHoverColor: '#687936',
+  breakpoints: {
+    xs: '0px',
+    sm: '320px',
+    md: '768px',
+    lg: '1440px',
+    xlg: '1920px',
+  },
   timeline: {
     background: '#ffffff',
     fontColor: '#223',
