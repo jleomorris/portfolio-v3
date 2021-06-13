3500fef30c5f65ee38ab5dac9df7adf5f4e48b3a
jleomorris <james-morris2010@hotmail.co.uk>
2021-06-13 03:15:56 +0100

    Comment unused console.log in useScroll

--- src/components/useScroll.js
+++ src/components/useScroll.js
@@ -1,15 +1,15 @@
-import { useInView } from "react-intersection-observer";
-import { useAnimation } from "framer-motion";
+import { useInView } from 'react-intersection-observer';
+import { useAnimation } from 'framer-motion';
 
 export const useScroll = () => {
   const controls = useAnimation();
   const [element, view] = useInView({ threshold: 0.2 });
-  console.log("in view", view);
+  //   console.log("in view", view);
 
   if (view) {
-    controls.start("show");
+    controls.start('show');
   } else {
-    controls.start("hidden");
+    controls.start('hidden');
   }
 
   return [element, controls];
