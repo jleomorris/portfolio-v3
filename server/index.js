eb67f38ccf3d6127b54fdbc6149af9741ca8b406
jleomorris <james-morris2010@hotmail.co.uk>
2021-06-14 02:59:27 +0100

    Add server index.js

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
+const GRAPHQL_KEY = process.env.REACT_APP_GITHUB_GRAPHQL_API_KEY;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(pino);
+
+// app.get('/api/greeting', (req, res) => {
+//   const name = req.query.name || 'World';
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+// });
+
+app.get('/api/keys', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ graphQL: GRAPHQL_KEY }));
+});
+
+app.listen(3001, () =>
+  console.log('Express server is running on localhost:3001')
+);
